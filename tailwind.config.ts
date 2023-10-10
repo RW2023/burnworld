0fe1706bdf834b9eaff2d7f751ed89631c2a875d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-10 06:34:32 -0400

    add custom color pallette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,20 @@ const config: Config = {
   },
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+       colors: {
+        'overlay': 'rgba(142, 125, 190, 0.5)', /* tropical-indigo */
+        'background': '#303633', /* onyx */
+        'button': '#fca311', /* orange-web */
+        'headline': '#ffffff', /* white */
+        'paragraph': '#8e7dbe', /* tropical-indigo */
+        'buttonText': '#303633', /* onyx */
+        'stroke': '#e5e5e5', /* platinum */
+        'main': '#8e7dbe', /* tropical-indigo */
+        'highlight': '#fca311', /* orange-web */
+        'secondary': '#ffffff', /* white */
+        'tertiary': '#fca311', /* orange-web */
       },
+     
     },
   },
    plugins: [require("daisyui")],
