8687216f2ef990da298cee26f00ddc64cc1aaf77
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-10 22:44:00 -0400

    create scoped css to troubleshoot css issue

--- src/Components/ArticleList.tsx
+++ src/Components/ArticleList.tsx
@@ -1,7 +1,7 @@
-
+/* ArticleList.tsx */
 import React from 'react';
 import Layout from './Layout';
-
+import styles from './ArticleList.module.css'; // Import CSS Module
 
 export type Article = {
   source: { id: string; name: string };
@@ -14,15 +14,17 @@ export type Article = {
   content: string;
 };
 
-
-
 type Props = {
   articles: Article[] | undefined;
 };
 
 const ArticleList: React.FC<Props> = ({ articles }) => {
   if (!articles) {
-    return <Layout title="News">Loading articles. Please wait it might be a big dump.........💩</Layout>;
+    return (
+      <Layout title="News">
+        Loading articles. Please wait it might be a big dump.........💩
+      </Layout>
+    );
   }
 
   const filteredArticles = articles.filter((article) => {
@@ -35,37 +37,32 @@ const ArticleList: React.FC<Props> = ({ articles }) => {
 
   return (
     <Layout title="Latest Headlines">
-      <div
-        className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4"
-        data-theme="black"
-      >
+      <div className={styles.container} data-theme="black">
         {filteredArticles.map((article, index) => (
-          <div key={index} className="p-4 rounded-lg shadow-xl">
-            <h2 className="text-2xl font-bold text-headline mb-2">
-              {article.title}
-            </h2>
-            <div className="flex justify-between mb-2">
-              <span className="text-paragraph">
+          <div key={index} className={styles.card}>
+            <h2 className={styles.title}>{article.title}</h2>
+            <div className={styles.info}>
+              <span>
                 By {article.author} | {article.source.name}
               </span>
-              <span className="text-paragraph">
+              <span>
                 Published on{' '}
                 {new Date(article.publishedAt).toLocaleDateString()}
               </span>
             </div>
             <img
               src={article.urlToImage}
-              alt={article.title} // add alt tag
+              alt={article.title}
               width={1080}
               height={1080}
               className="w-full rounded-lg mb-4"
             />
-            <p className="text-stroke mb-4">{article.description}</p>
+            <p className={styles.description}>{article.description}</p>
             <a
               href={article.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-highlight underline"
+              className={styles.link}
             >
               Read more
             </a>
