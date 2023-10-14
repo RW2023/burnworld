3c2c71b7fd0cf985bfc676ee03d0b456fc4aca8d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-14 08:16:36 -0400

    create new Navbar update navbar using daisy ui component

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,29 +2,82 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-6 bg-gray-800 sticky top-0 z-50">
-      <div className="flex items-center flex-shrink-0 text-headline mr-6">
-        <Link href="/" passHref>
-          <span className="font-bold text-xl tracking-tight cursor-pointer text-white hover:text-gray-300">
-            Home
-          </span>
-        </Link>
-      </div>
-      <div className="block lg:flex lg:items-center">
-        <div className="text-sm lg:flex-grow">
-          <Link href="/news" passHref>
-            <span className="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 cursor-pointer">
-              News
-            </span>
-          </Link>
-          <Link href="#" passHref>
-            <span className="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 cursor-pointer">
-              About
-            </span>
-          </Link>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/news">Headlines</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </nav>
+      <div className="navbar-center">
+        <a className="btn btn-ghost normal-case text-xl">Burn World News</a>
+      </div>
+      <div className="navbar-end">
+        <button type='button' title='button' className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+        <button type='button' aria-label='button' className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              />
+            </svg>
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
+      </div>
+    </div>
   );
 };
 
