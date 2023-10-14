21b040462e119121d781eafd02da171fe7be9e12
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-14 04:09:51 -0400

    update color

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between p-6 bg-gray-800 sticky top-0 z-50">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+      <div className="flex items-center flex-shrink-0 text-headline mr-6">
         <Link href="/" passHref>
           <span className="font-bold text-xl tracking-tight cursor-pointer text-white hover:text-gray-300">
             Home
