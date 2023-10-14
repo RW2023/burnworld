10afc1e620f80d738aca8d66473b3a88cb5e7deb
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-13 20:04:59 -0400

    udpate tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,30 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  mode: 'jit',  // Ensuring JIT mode is enabled
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  daisyui: {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   daisyui: {
     themes: true,
   },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'overlay': 'rgba(142, 125, 190, 0.5)', /* tropical-indigo */
-        'background': '#303633', /* onyx */
-        'button': '#fca311', /* orange-web */
-        'headline': '#ffffff', /* white */
-        'paragraph': '#8e7dbe', /* tropical-indigo */
-        'buttonText': '#303633', /* onyx */
-        'stroke': '#e5e5e5', /* platinum */
-        'main': '#8e7dbe', /* tropical-indigo */
-        'highlight': '#fca311', /* orange-web */
-        'secondary': '#ffffff', /* white */
-        'tertiary': '#fca311', /* orange-web */
+        overlay: 'rgba(142, 125, 190, 0.5)' /* tropical-indigo */,
+        background: '#303633' /* onyx */,
+        button: '#fca311' /* orange-web */,
+        headline: '#ffffff' /* white */,
+        paragraph: '#8e7dbe' /* tropical-indigo */,
+        buttonText: '#303633' /* onyx */,
+        stroke: '#e5e5e5' /* platinum */,
+        main: '#8e7dbe' /* tropical-indigo */,
+        highlight: '#fca311' /* orange-web */,
+        secondary: '#ffffff' /* white */,
+        tertiary: '#fca311' /* orange-web */,
       },
     },
   },
-  plugins: [require("daisyui")],
-};
-
-export default config;
+plugins: [require("daisyui")],
+}
