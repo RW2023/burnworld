2ab127442ff7c15a72d599f33c7ddcf6ce62069e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-13 23:11:41 -0400

    update navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6 sticky top-0 z-50">
+    <nav className="flex items-center justify-between p-6 bg-gray-800 sticky top-0 z-50">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link href="/" passHref>
-          <span className="font-semibold text-xl tracking-tight cursor-pointer">
+          <span className="font-bold text-xl tracking-tight cursor-pointer text-white hover:text-gray-300">
             Home
           </span>
         </Link>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div className="block lg:flex lg:items-center">
         <div className="text-sm lg:flex-grow">
           <Link href="/news" passHref>
             <span className="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 cursor-pointer">
