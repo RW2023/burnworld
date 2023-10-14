b43b051c0fa861ed43b5225cbf884c2e27ad52e1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-14 04:56:20 -0400

    update color pallette Remove the color pallette from the globals.css file did do anything when they were the same but I realize now to keep global stuff in the tailwind config file. easier to manage.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   daisyui: {
+  daisyui: {
     themes: true,
   },
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        overlay: 'rgba(142, 125, 190, 0.5)' /* tropical-indigo */,
-        background: '#303633' /* onyx */,
-        button: '#fca311' /* orange-web */,
-        headline: '#ffffff' /* white */,
-        paragraph: '#8e7dbe' /* tropical-indigo */,
-        buttonText: '#303633' /* onyx */,
-        stroke: '#e5e5e5' /* platinum */,
-        main: '#8e7dbe' /* tropical-indigo */,
-        highlight: '#fca311' /* orange-web */,
-        secondary: '#ffffff' /* white */,
-        tertiary: '#fca311' /* orange-web */,
+        overlay: 'rgba(43, 45, 66, 0.5)', // --space-cadet with 50% opacity
+        background: '#2b2d42', // --space-cadet
+        button: '#f8f32b', // --aureolin
+        headline: '#ffffff', // --white
+        paragraph: '#8d99ae', // --cool-gray
+        buttonText: '#2b2d42', // --space-cadet
+        stroke: '#8d99ae', // --cool-gray
+        main: '#f8f32b', // --aureolin
+        highlight: '#f8f32b', // --aureolin
+        secondary: '#8d99ae', // --cool-gray
+        tertiary: '#000000', // --black
       },
     },
   },
-plugins: [require("daisyui")],
-}
+  plugins: [require('daisyui')],
+};
