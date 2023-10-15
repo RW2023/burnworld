e7d4c768f091556cd668141f78ce34781404dc48
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-15 01:58:44 -0400

    update footer background

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-background-300 text-paragraph">
       <aside>
         <p className='text-headline'>Copyright © 2023 - All right reserved by Burn News Aggregator</p>
       </aside>
