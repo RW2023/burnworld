ddc95ba7892252a8d819b56d95c77ea5a0e2be4d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-23 09:57:21 -0400

    update canadian filter to allow articles with no images

--- src/pages/canada.tsx
+++ src/pages/canada.tsx
@@ -11,16 +11,15 @@ const CanadianNews: React.FC = () => {
         const response = await axios.get('/api/news?country=ca');
         const allArticles = response.data.articles;
 
-        // // Filter articles based on criteria
-        // const articlesWithBoth = allArticles.filter(
-        //   (article: Article) => article.description && article.urlToImage,
-        // );
+        // Filter articles based on criteria
+        const articlesWithBoth = allArticles.filter(
+            (article: Article) => article.description && article.urlToImage,
+        );
         // const articlesWithNoImage = allArticles.filter(
         //   (article: Article) => article.description && !article.urlToImage,
         // );
 
         // Combine the filtered articles, placing articles with no image at the end
-       
         setArticles(allArticles);
       } catch (error) {
         if (error instanceof Error) {
