396cb33336577be4d9e8a8b55a8f58f970c26e4f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-24 10:42:38 -0400

    clean up and simply component remove some unused code and comments and remove the filtering

--- src/pages/us/index.tsx
+++ src/pages/us/index.tsx
@@ -1,44 +1,28 @@
+// pages/news.tsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import ArticleList, { Article } from '../../Components/ArticleList';
+import ArticleList, { Article } from '../../Components/ArticleList'; 
 import Layout from './Layout';
 
-const CanadianNews: React.FC = () => {
-  const [articles, setArticles] = useState<Article[]>([]); // Set default state to an empty array
+const News: React.FC = () => {
+  const [articles, setArticles] = useState<Article[] | undefined>(undefined); 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/api/news?country=us');
-        const allArticles = response.data.articles;
-
-        // Filter articles based on criteria
-        const articlesWithBoth = allArticles.filter(
-          (article: Article) => article.description && article.urlToImage,
-        );
-        // const articlesWithNoImage = allArticles.filter(
-        //   (article: Article) => article.description && !article.urlToImage,
-        // );
-
-        // Combine the filtered articles, placing articles with no image at the end
-        setArticles(allArticles);
+        const response = await axios.get('/api/news');
+        setArticles(response.data.articles);
       } catch (error) {
-        if (error instanceof Error) {
-          console.error('Error fetching Canadian news data:', error.message);
-        } else {
-          console.error('An unknown error occurred:', error);
-        }
+        console.error('Error fetching news data:', error);
       }
     };
-
     fetchData();
   }, []);
 
   return (
-    <Layout>
-      <ArticleList articles={articles} />
-    </Layout>
-  );
+  <Layout>
+    <ArticleList articles={articles} />
+  </Layout>);
 };
 
-export default CanadianNews;
+export default News;
