53e5b6e29593de7d2725ac2cfc95ecc229440702
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-24 15:55:47 -0400

    stick footer to bottom of page

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-background-300 text-paragraph">
+    <footer className="footer footer-center p-4 bg-background-300 text-paragraph fixed bottom-0 w-full">
       <aside>
         <p className='text-headline'>Copyright © 2023 - All right reserved by Burn News Aggregator</p>
       </aside>
