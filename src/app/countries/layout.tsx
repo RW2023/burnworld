416ae45880ff09090adbefae1fc120735fa0bda9
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-24 07:04:04 -0400

    create countries layout

---
+++ src/app/countries/layout.tsx
@@ -0,0 +1,19 @@
+import CountriesNav from '@/Components/CountriesNav';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" data-theme="black">
+      <head>
+        <title>News By Country</title>
+      </head>
+      <body>
+        <CountriesNav />
+        {children}
+      </body>
+    </html>
+  );
+}
