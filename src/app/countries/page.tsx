3666a5ea488008c2803489222edbf062d69891a3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-24 18:24:48 -0400

    update countries just an empty page for now

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { FC } from 'react';
 import Layout from '../../Components/Layout';
+import NextImage from 'next/image';
 
 
 
@@ -9,8 +10,49 @@ interface Props {}
 const Index: FC<Props> = (): JSX.Element => {
   return (
     <Layout title={'Countries'}>
-      <div className='min-h-screen'>
-       <h1>Countries</h1>
+      <div className="min-h-screen bg-background">
+        {/* <h1 className="text-headline">Countries</h1>
+        <div className="min-h-screen">
+          <div className="country p-4 bg-main rounded-lg shadow-lg mb-6">
+            <NextImage
+              src="/images/burningGlobe.png"
+              alt="other country"
+              width={500}
+              height={500}
+              className="rounded-lg"
+            />
+            <div className="text-container mt-4">
+              <h2 className="text-highlight">US</h2>
+              <p className="text-paragraph">US Headlines</p>
+            </div>
+          </div>
+          <div className="country p-4 bg-main rounded-lg shadow-lg mb-6">
+            <NextImage
+              src=""
+              alt="Country 2"
+              width={500}
+              height={500}
+              className="rounded-lg"
+            />
+            <div className="text-container mt-4">
+              <h2 className="text-highlight">Country 2</h2>
+              <p className="text-paragraph">Description of Country 2</p>
+            </div>
+          </div>
+          <div className="country p-4 bg-main rounded-lg shadow-lg mb-6">
+            <NextImage
+              src=""
+              alt="Country 3"
+              width={500}
+              height={500}
+              className="rounded-lg"
+            />
+            <div className="text-container mt-4">
+              <h2 className="text-highlight">Country 3</h2>
+              <p className="text-paragraph">Description of Country 3</p>
+            </div>
+          </div>
+        </div> */}
       </div>
     </Layout>
   );
