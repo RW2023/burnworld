e711676a2b1f28d3e7e577d8c378b67ae740864a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-24 07:04:19 -0400

    create countries nav

---
+++ src/Components/CountriesNav.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+
+type Props = {}
+
+export default function CountriesNav({}: Props) {
+  return (
+    <div>
+      {' '}
+    <nav className="sticky top-0 flex flex-row justify-center bg-gray-800 text-white p-4 text-center">
+        <ul className="flex flex-row space-x-4 justify-center">
+          <li>
+            <Link href="/news">US</Link>
+          </li>
+          <li>
+            <Link href="/canada">Canada</Link>
+          </li>
+            <li>
+                <Link href="/jamaica">Jamaica</Link>
+            </li>
+          <li>
+            <Link href="/news">Headlines</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
