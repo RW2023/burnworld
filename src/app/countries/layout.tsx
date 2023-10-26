94c93fff837c5e6bacd698178fd2ecc78c83dbcd
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-26 07:23:52 -0400

    add favicon layout was not applied to article ArticleList I had to apply the favicon to the layout for the countries route I will refactor the code later to move the elements I don't want to be global out of the layout once thats done I will wrap the country component in the layout this is just a work around for now

--- src/app/countries/layout.tsx
+++ src/app/countries/layout.tsx
@@ -8,6 +8,15 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="black">
       <head>
+        <link
+          rel="icon"
+          href="/favicon_io/favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link rel="apple-touch-icon" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+
         <title>News By Country</title>
       </head>
       <body>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,17 @@ class MyDocument extends Document {
             integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
             crossOrigin="anonymous"
           />
+          <link
+            rel="icon"
+            href="/favicon_io/favicon-32x32.png"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/favicon_io/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/favicon_io/site.webmanifest" />
         </Head>
 
         <body>
