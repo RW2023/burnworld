4b2e76ed521af4481af3053c7fd133ea012eaab2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-28 08:26:32 -0400

    clean up code remove comments

--- src/app/countries/us/Layout.tsx
+++ src/app/countries/us/Layout.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 interface Props {
@@ -7,13 +8,13 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div>
-      <title>US News</title>
-      {/* Add your header component here */}
-      <main>
+     
+      <title>US News</title> 
+    
+      <main>  
         <h1 className="text-3xl text-center text-font-bold">US News</h1>{' '}
         {children}
       </main>
-      {/* Add your footer component here */}
     </div>
   );
 };
