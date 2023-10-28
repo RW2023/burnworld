ff05e0a938ecafb32e2052283602c374db16ddad
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-28 06:17:13 -0400

    add meta data info to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,35 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import './globals.css';
+import Head from 'next/head';
+import type { ReactNode } from 'react';
 
-
-
-
-export const metadata: Metadata = {
-  title: 'Burn World News',
-  description: 'News Headlines from around the world',
-  keywords: 'news, headlines, world, international, local, politics, business',
-  authors: [{ name: 'Ryan Wilson' }],
+interface RootLayoutProps {
+  children: ReactNode;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        <title>Burn World News</title>
+        <meta
+          name="description"
+          content="News Headlines from around the world"
+        />
+        <meta
+          name="keywords"
+          content="news, headlines, world, international, local, politics, business"
+        />
+        <meta name="author" content="Ryan Wilson" />
+        <link
+          rel="icon"
+          href="/favicon_io/favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link rel="apple-touch-icon" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+      </Head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
