a2c31ed111aef2eb8ce5b55fdf1f504e26143733
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-15 07:12:13 -0500

    Refactor hero section in page component. change page daisy theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,20 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div
+      data-theme="business"
       className="hero min-h-screen"
       style={{
-        backgroundImage:
-          'url(/hero.png)',
+        backgroundImage: 'url(/hero.png)',
       }}
     >
       <div className="hero-overlay"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Burn World News</h1>
-        <h2>Headlines From Around The World</h2>
-          <Link
-          href="/countries/us"
-          ><button className="btn btn-primary">Read News</button></Link>
+          <h2>Headlines From Around The World</h2>
+          <Link href="/countries/us">
+            <button className="btn btn-primary">Read News</button>
+          </Link>
         </div>
       </div>
     </div>
